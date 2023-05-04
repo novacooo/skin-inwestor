9ad642c34625f5e74f9e63276fdca5640d80826a
Jacek Nowak <jaceknowak226@gmail.com>
2023-05-04 23:15:54 +0200

    ci: add eslint-plugin-tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="flex min-h-screen flex-col items-center bg-zinc-950 p-6">
+      <h1 className="text-center text-6xl font-black tracking-wider text-zinc-300">
+        Skin Inwestor
+      </h1>
+      <h2 className="mt-3 text-center text-2xl font-semibold tracking-wide text-zinc-500">
+        by @novacooo
+      </h2>
     </main>
   );
 }
